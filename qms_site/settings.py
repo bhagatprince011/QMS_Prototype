1d22d3a0cd3615a417229644d65a7e0494887f24
BhagatPrince <bhagatprince011@gmail.com>
2025-01-01 02:55:33 +0530

    supabase for hosting database

--- qms_site/settings.py
+++ qms_site/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'qms_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Prince@111',
+        'HOST': 'db.locvimidtdtjndqpmszp.supabase.co',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
